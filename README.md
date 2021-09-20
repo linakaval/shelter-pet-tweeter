776b06df446d17ad7c964562ccf4b7fcd133e5cc
Lina Kaval <linakaval@Linas-MacBook-Pro.local>
2021-09-19 20:46:45 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # shelter-pet-tweeter
-For my shelter animal pet of the day tweeter
+About: 
+A simple Twitter automation that tweets details about a single pet that is being housed at the local animal shelter I work with, Cincinnati Animal CARES. My hope is to be able to make this tweet from my account (@leankeuisine on Twitter) once a day at noon. 
+
+Other Notes:
+This requires the creation of a separate Python module called "auth_credentials.py", where the Twitter API credentials such as consumer key and access tokens are stored. I simply have a function called twitter() that returns the consumer key, consumer secret, access token, and access token secret as a dictionary, which I reference within pet_of_the_day.py
---
+++ pet_of_the_day.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import tweepy
+import os
+import sys
+sys.path.append("/Users/linakaval/Documents/Github/")
+import auth_credentials #module with credentials
+
+DOG_URL = 'https://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals2.aspx?species=dog&gender=All&agegroup=All&location=&site=&onhold=All&orderby=ID&colnum=5&authkey=ho44bl6bmphbs21iior7k671v2gh2cf8r70636k2i7rcvs1br0&recAmount=&detailsInPopup=Yes&featuredPet=Include&stageID='
+CAT_URL = 'https://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals2.aspx?species=cat&gender=All&agegroup=All&location=&site=&onhold=All&orderby=ID&colnum=5&authkey=ho44bl6bmphbs21iior7k671v2gh2cf8r70636k2i7rcvs1br0&recAmount=&detailsInPopup=Yes&featuredPet=Include&stageID='
+OTHER_URL = 'https://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals2.aspx?species=other&gender=All&agegroup=All&location=&site=&onhold=All&orderby=ID&colnum=5&authkey=ho44bl6bmphbs21iior7k671v2gh2cf8r70636k2i7rcvs1br0&recAmount=&detailsInPopup=Yes&featuredPet=Include&stageID='
+
+def getAnimals(url, animals):
+    with requests.Session() as s:
+        page = s.get(url)
+        if page.status_code == 200:
+            
+            soup = BeautifulSoup(page.content, 'html.parser')
+            divs  = soup.findAll("div", {'class': 'list-item'})
+            #print(divs)
+
+            for div in divs:
+                name = div.find("div", {'class': 'list-animal-name'}).find('a').text
+                species = div.find("div", {'class': 'list-anima-species'}).text
+                sex = div.find("div", {'class': 'list-animal-sexSN'}).text
+                breed = div.find("div", {'class': 'list-animal-breed'}).text
+                age = div.find("div", {'class': 'list-animal-age'}).text
+                picture = div.find("div", {'class': 'list-animal-photo-block'}).find("img")['src']
+                #print(name, species, sex, breed, age)
+                animals.append([name, species, sex, breed, age, picture])  
+
+
+def main():
+    cred = auth_credentials.twitter()
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(cred['CONSUMER_KEY'], cred['CONSUMER_SECRET'])
+    auth.set_access_token(cred['ACCESS_TOKEN'], cred['ACCESS_TOKEN_SECRET'])
+
+    # Create API object
+    api = tweepy.API(auth)
+    current_animals = []
+    tempPic = 'temp_pic.jpg'
+
+    getAnimals(DOG_URL, current_animals)
+    getAnimals(CAT_URL, current_animals)
+    getAnimals(OTHER_URL, current_animals)
+
+    randomPet = current_animals[random.randint(0, len(current_animals))]
+
+    with requests.Session() as s:
+        pic = requests.get(randomPet[5], stream=True)
+        if pic.status_code == 200:
+            with open(tempPic, 'wb') as image:
+                for chunk in pic.iter_content():
+                    image.write(chunk)
+            api.update_with_media(tempPic, status='CACHS Pet of the Day\nName: {}\nSpecies: {}\nGender: {}\nBreed: {}\nAge: {}\nAdopt this cute animal today!'.format(randomPet[0], randomPet[1], randomPet[2], randomPet[3], randomPet[4]))
+        os.remove(tempPic)
+    
+    print(randomPet)
+
+    
+if __name__ == "__main__":
+    main()
